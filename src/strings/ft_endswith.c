935228450f22e7bd5bbb2c8f7f65dd34e083a112
5IGI0 <5IGI0@protonmail.com>
2022-04-13 10:26:52 +0200

    Add length check to ft_endswith

--- src/strings/ft_endswith.c
+++ src/strings/ft_endswith.c
@@ -6,7 +6,7 @@
 /*   By: elorenze <elorenze@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 01:14:40 by elorenze          #+#    #+#             */
-/*   Updated: 2022/04/07 01:34:51 by elorenze         ###   ########.fr       */
+/*   Updated: 2022/04/13 10:25:34 by elorenze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 int	ft_endswith(const char *str, const char *end)
 {
 	size_t	s;
+	size_t	e;
 
-	s = ft_strlen(end);
+	e = ft_strlen(end);
+	s = ft_strlen(str);
+	if (e > s)
+		return (-1);
 	return (ft_memcmp(str + ft_strlen(str) - s, end, s));
 }
