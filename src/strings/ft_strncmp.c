52f3b533db6bb10f585ece573336dc0f46e0e789
5IGI0 <5IGI0@protonmail.com>
2022-04-19 08:00:08 +0200

    Modify ft_strncmp

--- src/strings/ft_strncmp.c
+++ src/strings/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: elorenze <elorenze@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 18:44:50 by elorenze          #+#    #+#             */
-/*   Updated: 2022/03/01 00:35:30 by elorenze         ###   ########.fr       */
+/*   Updated: 2022/04/19 07:46:22 by elorenze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 int	ft_strncmp(char *s1, char *s2, size_t n)
 {
-	if (n == 0 || s1 == NULL || s2 == NULL)
+	if (n == 0)
 		return (0);
+	if (s1 == NULL || s2 == NULL)
+	{
+		if (s1 < s2)
+			return (-256);
+		return (256);
+	}
 	n--;
 	while (n && s1[0] && s1[0] == s2[0])
 	{
